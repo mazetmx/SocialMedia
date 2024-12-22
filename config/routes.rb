390830482689732e0a20cf86b4b6813d58f57ea5
mazetmx <mazen.waleed.161299@gmail.com>
2024-12-22 16:14:09 +0200

    Added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +11,10 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  authenticated :user do
+    root to: "user#index", as: :authenticated_user
+  end
+
+  root "home#index"
 end
